16921b63b65ad8a26a842d0d9f5b00a3ceb27670
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-18 23:04:16 +0200

    Created Controls component with his html

---
+++ src/Components/Controls.js
@@ -0,0 +1,11 @@
+
+const Controls = () => {
+    return (
+        <section class="controls">
+        <p class="info">0 gentlemen pointing at you</p>
+        <button class="button button--select">Select all</button>
+      </section>
+    );
+};
+
+import default Controls;
